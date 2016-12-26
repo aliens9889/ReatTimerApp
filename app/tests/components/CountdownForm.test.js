bb6f9d990074f60d827c4ca5142e7e05bae4a51c
aliens9889 <alienx9889@gmail.com>
2016-12-26 12:35:16 -0400

    feat(CountdownForm.test): Adding Tests on the Script

--- app/tests/components/CountdownForm.test.js
+++ app/tests/components/CountdownForm.test.js
@@ -1,3 +1,22 @@
-"use strict";
+'use strict';
+
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtil = require('react-addons-test-utils');
+var $ = require('jQuery');
+
+var CountdownForm = require('CountdownForm');
+
+describe('CountdownForm', function () {
+   it('should exist', function () {
+      expect(CountdownForm).toExist();
+   });
+
+   it('should call onSetCountdown if valid seconds entered', function () {
+      var spy = expect.createSpy();
+      var countdownForm = TestUtil.renderIntoDocument();
+   });
+});
 
 //# sourceMappingURL=CountdownForm.test.js.map
